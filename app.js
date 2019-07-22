7e6cba7f492521af3107c8e77f4f3e64f088a34c
Utsav69 <53001459+Utsav69@users.noreply.github.com>
2019-07-22 22:40:26 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ devices.push({ user: "A", name: "A's device"});
 localStorage.setItem('devices', JSON.stringify(devices));
 
 
-devices.forEach(function(device) {
-    $('#devices tbody').append('
+devices.forEach(function(device) 
+{
+    $('#devices tbody').append(`
       <tr>
         <td>${device.user}</td>
         <td>${device.name}</td>
-      </tr>'
-    );
-});
+      </tr>`
+      );
+  });
 
 
-$('#add-device').on('click', function() {
+  $('#add-device').on('click', function() {
     const newuser = $('#user').val();
     const newname = $('#name').val();
     devices.push({ user: newuser, name: newname });
     localStorage.setItem('devices', JSON.stringify(devices));
     location.href = '/';
-});
+  });
 
 
 $('#send-command').on('click', function() {
     const command = $('#command').val();
     console.log('command is: ${command}');
 });
+
+
+location.href = '/';
